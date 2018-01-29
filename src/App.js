ffcb7ccec7eecf98c1b289d386ccfdd1e17455f7
mnfmnfm <mnfmnfm@gmail.com>
2018-01-29 15:31:19 -0800

    all questions added

--- src/App.js
+++ src/App.js
@@ -17,15 +17,23 @@ const categories = [
       ["The word we use to create a class that is a Component", "What is extends?"],
       ["This is why you would need to pass a callback function as the argument to setState instead of an object", "What is updating the state based on the current state?"],
     ]],
-    ["The Component Lifecycle", [
+    ["The Flow of Data", [
+      ["The function to change state within a component", "What is this.setState()?"],
+      ['The expression used to read a prop called \'banana\' in a component', 'What is this.props.banana?'],
+      ['The expression used to read a key called "apple" in a component\'s state', 'What is this.state.apple?'],
+      ['The direction that props are passed', 'What is parent to child?'],
+    ]],
+    ["Component Lifecycle", [
       ["This is the first method called when a component is created", "What is the constructor?"],
       ["This is the first line of any component constructor", "What is super(props)?"],
       ["This is the method called when a component is getting new props", "What is componentWillReceiveProps()?"],
+      ["These are 3 React lifecycle methods", "What are componentWillReceiveProps, componentWillMount, componentDidMount? (Also accept render, etc.)"],
       ["This is the correct method in which to put an initial AJAX request within a component", "What is componentDidMount()?"],
     ]],
     ["Potpourri", [
       ["The result of running this.setState({}) when the state was {information: \"Hello!\"}", "What is the state not changing, or still being {information: \"Hello!\"}?"],
       ["The characters we use to escape JavaScript within the HTML-ish code in a React file", "What are curly braces {}?"],
+      ["The method you must call on new functions defined within a component to ensure it maintains the reference to the correct this", "What is .bind()?"],
       ["The exact code for a render function that creates a div with a class of information", "What is render() {return(<div className=\"information\"></div>);}?"],
       ["Two ways we can use conditionals in our render code", "What are using if statements around our return statements and using ternary within our return statement?"],
     ]],
